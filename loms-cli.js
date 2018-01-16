93571954dfe258c82f84631b9efd0f22bec7166e
tyrealgray <tyrealgray@ymail.com>
2018-01-17 00:02:10 +0100

    update to 0.0.2, finished init cmd feature

--- loms-cli.js
+++ loms-cli.js
@@ -1,12 +1,31 @@
-
+#!/usr/bin/env node
 const program = require('commander');
-const download = require('download');
+const clone = require('git-clone');
+const path = require('path');
+
+const Spinner = require('cli-spinner').Spinner;
+const loadingStr = new Spinner('Clone LegendOfMountainSea to local %s');
 
 program
-	.version('0.0.1')
-	.command('init', 'Setup project.')
-	.action(function(){
-		download('https://github.com/SkyHarp/LegendOfMountainSea/archive/master.zip', process.cwd() + '/',{ extract: true, strip: 1, mode: '666', headers: { accept: 'application/zip' }});
+	.version('0.0.2')
+	.description('LOMS Development CLI');
+
+program
+	.command('init')
+	.description('Setup project.')
+	.action(function () {
+		
+		loadingStr.setSpinnerString(18);
+		loadingStr.start();
+		
+		clone('https://github.com/SkyHarp/LegendOfMountainSea.git', path.join(process.cwd(), 'LegendOfMountainSea'),
+			{checkout: 'master'},
+			function () {
+				loadingStr.stop();
+				console.log('');
+				console.log('Clone finished!');
+				
+			});
 	});
 
 program.parse(process.argv);
