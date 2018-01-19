41d97b5c97eca449e9bdd444a1bfe72049d01f8e
tyrealgray <tyrealgray@ymail.com>
2018-01-19 21:23:46 +0100

    add nwjs download feature in init cmd

--- loms-cli.js
+++ loms-cli.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
+const exec = require('child_process').exec;
+
 const program = require('commander');
 const clone = require('git-clone');
+const download = require('download');
 const path = require('path');
-
 const Spinner = require('cli-spinner').Spinner;
-const loadingStr = new Spinner('Clone LegendOfMountainSea to local %s');
 
 program
 	.version('0.0.2')
@@ -15,15 +16,40 @@ program
 	.description('Setup project.')
 	.action(function () {
 		
-		loadingStr.setSpinnerString(18);
-		loadingStr.start();
+		const cloneLoadingStr = new Spinner('Clone LegendOfMountainSea to local %s');
+		cloneLoadingStr.setSpinnerString(18);
+		cloneLoadingStr.start();
 		
 		clone('https://github.com/SkyHarp/LegendOfMountainSea.git', path.join(process.cwd(), 'LegendOfMountainSea'),
 			{checkout: 'master'},
 			function () {
-				loadingStr.stop();
-				console.log('');
-				console.log('Clone finished!');
+				cloneLoadingStr.stop();
+				console.log('','Clone finished!');
+				
+				const downloadingStr = new Spinner('download nwjs client to local %s');
+				downloadingStr.setSpinnerString(18);
+				downloadingStr.start();
+				
+				let downloadURL = null;
+				
+				if(/^win/.test(process.platform)){
+					downloadURL = 'https://nwjs-v0.27.0-win-x64.zip';
+				}else {
+					downloadURL = 'https://nwjs-v0.27.0-osx-x64.zip';
+				}
+				
+				download(downloadURL, path.join(process.cwd(), 'LegendOfMountainSea') ).then(() => {
+					downloadingStr.stop();
+					console.log('','download finished!');
+					
+					exec('cd LegendOfMountainSea/LOMS && npm i', function(error, stdout, stderr) {
+						console.log('stdout: ' + stdout);
+						console.log('stderr: ' + stderr);
+						if (error !== null) {
+							console.log('exec error: ' + error);
+						}
+					});
+				});
 				
 			});
 	});
