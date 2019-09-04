045a6bb6d7bd386c4d7cf4cbe762d359b35ef510
Tianrui Guo <tyrealgray@ymail.com>
2019-09-04 15:50:27 +0100

    Update clientDownloadUtil.js

--- src/clientDownloadUtil.js
+++ src/clientDownloadUtil.js
@@ -6,15 +6,27 @@ const fsExtra = require('fs-extra');
 function downloadClientAndSDK(){
 
     return new Promise((resolve, reject) => {
-        const downloadPlatform = /^win/.test(process.platform) ? 'win' : 'osx';
+        let downloadPlatform = null;
+	    switch (process.platform) {
+	      case 'win32':
+		downloadPlatform = 'win';
+		break;
+	      case 'linux':
+		downloadPlatform = 'linux';
+		break;
+	      default:
+		downloadPlatform = 'osx';
+		break;
+	    }
 
-        const clientName = `nwjs-v0.30.1-${downloadPlatform}-x64`,
-            sdkName = `nwjs-sdk-v0.30.1-${downloadPlatform}-x64`;
+	    const clientName = `nwjs-v0.30.1-${downloadPlatform}-x64`,
+	      sdkName = `nwjs-sdk-v0.30.1-${downloadPlatform}-x64`,
+	      fileName = /^linux/.test(downloadPlatform) ? 'tar.gz' : 'zip';
 
-        const downloadURL = `https://dl.nwjs.io/v0.30.1/`;
+	    const downloadURL = `https://dl.nwjs.io/v0.30.1/`;
 
-        const clientDownloadURL = `${downloadURL}${clientName}.zip`,
-            sdkDownloadURL = `${downloadURL}${sdkName}.zip`;
+	    const clientDownloadURL = `${downloadURL}${clientName}.${fileName}`,
+	      sdkDownloadURL = `${downloadURL}${sdkName}.${fileName}`;
 
 	    if(fsExtra.existsSync(path.join(process.cwd(),clientName) )&& fsExtra.existsSync(path.join(process.cwd(),sdkName))){
 		    return resolve();
