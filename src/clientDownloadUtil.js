14a43f30ac29657dbc6340d9e967a927f822f851
TyrealGray <tyrealgray@ymail.com>
2019-09-11 23:22:07 +0100

    remove header for zip

--- src/clientDownloadUtil.js
+++ src/clientDownloadUtil.js
@@ -3,47 +3,46 @@ const path = require('path');
 
 const fsExtra = require('fs-extra');
 
-function downloadClientAndSDK(){
+function downloadClientAndSDK() {
 
-    return new Promise((resolve, reject) => {
-        let downloadPlatform = null;
-	    switch (process.platform) {
-	      case 'win32':
-		downloadPlatform = 'win';
-		break;
-	      case 'linux':
-		downloadPlatform = 'linux';
-		break;
-	      default:
-		downloadPlatform = 'osx';
-		break;
-	    }
+	return new Promise((resolve, reject) => {
+		let downloadPlatform = null;
+		switch (process.platform) {
+			case 'win32':
+				downloadPlatform = 'win';
+				break;
+			case 'linux':
+				downloadPlatform = 'linux';
+				break;
+			default:
+				downloadPlatform = 'osx';
+				break;
+		}
 
-	    const clientName = `nwjs-v0.30.1-${downloadPlatform}-x64`,
-	      sdkName = `nwjs-sdk-v0.30.1-${downloadPlatform}-x64`,
-	      fileName = /^linux/.test(downloadPlatform) ? 'tar.gz' : 'zip';
+		const clientName = `nwjs-v0.30.1-${downloadPlatform}-x64`,
+			sdkName = `nwjs-sdk-v0.30.1-${downloadPlatform}-x64`,
+			fileName = /^linux/.test(downloadPlatform) ? 'tar.gz' : 'zip';
 
-	    const downloadURL = `https://dl.nwjs.io/v0.30.1/`;
+		const downloadURL = `https://dl.nwjs.io/v0.30.1/`;
 
-	    const clientDownloadURL = `${downloadURL}${clientName}.${fileName}`,
-	      sdkDownloadURL = `${downloadURL}${sdkName}.${fileName}`;
+		const clientDownloadURL = `${downloadURL}${clientName}.${fileName}`,
+			sdkDownloadURL = `${downloadURL}${sdkName}.${fileName}`;
 
-	    if(fsExtra.existsSync(path.join(process.cwd(),clientName) )&& fsExtra.existsSync(path.join(process.cwd(),sdkName))){
-		    return resolve();
-	    }
+		if (fsExtra.existsSync(path.join(process.cwd(), clientName)) && fsExtra.existsSync(path.join(process.cwd(), sdkName))) {
+			return resolve();
+		}
 
-        Promise.all([
-            clientDownloadURL,
-            sdkDownloadURL
-        ].map(x => download(x, path.join(process.cwd()), {
-            extract: true,
-            headers: {accept: 'application/zip'}
-        }).catch(error => {
-            return reject(error);
-        }))).then(()=>{
-            return resolve();
-        });
-    });
+		Promise.all([
+			clientDownloadURL,
+			sdkDownloadURL
+		].map(x => download(x, path.join(process.cwd()), {
+			extract: true,
+		}).catch(error => {
+			return reject(error);
+		}))).then(() => {
+			return resolve();
+		});
+	});
 }
 
 exports.downloadClientAndSDK = downloadClientAndSDK;
